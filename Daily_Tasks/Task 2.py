f79146eaef47750953ae969477ad673ab0950f9b
saiteja 4444 <saiteja.tns@gmail.com>
2024-06-23 20:48:42 +0530

    initial commit

---
+++ Daily_Tasks/Task 2.py
@@ -0,0 +1,11 @@
+# # write a simple and compound intrest program.
+p=int(input("Enter a value:"))
+t=int(input("Enter a value:"))
+r=int(input("Enter a value:"))
+print(p*t*r%100)
+
+
+# Area of Triangle 
+Breath = int(input("Enter your value:"))
+Height = int(input("Enter your value:"))
+print(Breath*Height*0.5)
