cafe6b89e90d88a1d2179b87f5e023768731fbce
saiteja 4444 <saiteja.tns@gmail.com>
2024-06-23 11:58:56 +0530

    intial commit

--- Python_Course/Day 2.py
+++ Python_Course/Day 2.py
@@ -1,2 +1,36 @@
 #                                           VARIABLES AND DATA TYPES !
 
+# what is comment?
+"""
+comments are the lines that are exit in python program and that are 
+ignored by the compiler or interpreter.
+
+- It is mainly divided into two types:
+1. Single line comments - it is denoted by hash symbol (#)
+2. Multi line comments  - it is denoted by triple quotes (""" """, ''' ''')
+"""
+
+
+# Variable!
+""" 
+Python variable are the containers which stores the value. It is not statically based.
+variable is a name given to memory location.
+"""
+
+# variable Rules !
+"""
+- A variable must name start with letter or Underscore(_).
+- A variable doesn't start with number or symbol (@,#,$,%).
+- Variable name are case sensitive.
+- The reseverd (Keywords) words cannot be used as variable name.
+"""
+
+# Data Types!
+""" 
+Here data Types are 5 types.
+1.Dictinories
+2.Numerical
+3.Boolean
+4.Set
+5.Sequence
+"""
