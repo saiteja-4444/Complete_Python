c319182faae020d914c8aa695caa1dd185f3e2d8
NADENDLA SAI TEJA <140083199+saiteja-4444@users.noreply.github.com>
2024-06-25 10:32:41 +0530

    Update Commit

--- Python_Course/Day 3.py
+++ Python_Course/Day 3.py
@@ -1,1 +1,20 @@
+#                                       OPERATORS AND CONTROL STATEMENT
+
+# what is operator ?
+"""
+Operations are speciall symbol or keyword used to perfrom operation on variable and values. 
+"""
+# Types of operators
+"""
+1.Arithmetic operator
+2.Assignment operator
+3.Logical operator
+4.Relational operator 
+5.Membership operator
+6.Bitwise operator
+7.Identify operator
+
+
+# Assignment operator
+
 
