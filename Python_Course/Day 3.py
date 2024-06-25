494af25d476154066f894f9471acfd912fcafd72
NADENDLA SAI TEJA <140083199+saiteja-4444@users.noreply.github.com>
2024-06-25 20:17:17 +0530

    Update Commt

--- Python_Course/Day 3.py
+++ Python_Course/Day 3.py
@@ -15,12 +15,55 @@ Operations are speciall symbol or keyword used to perfrom operation on variable 
 7.Identify operator
 """
 
-
-# Assignment operator
+# Arithmetic operator
 
 a=int(input("Enter a number:"))
 b=int(input("Enter a number:"))
 print("Addition",a+b)
-print("subtraction"a-b)
-print("Multiplication"a*b)
-print("Division"a/b)
+print("subtraction",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+
+
+# Assignment operator
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+a+=b  # a = a+b
+print(a)
+
+
+
+# Realational operator
+
+a=5
+b=2
+print(a==b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+print(a<b)
+print(a>b)
+
+
+#logical operator
+a=9.99
+print(a<10 and a>9)
+print(a<9 or a>10)
+print(a!= 0)
+
+
+
+# Membership operator
+a=[13,566,342,625,62,262,721,989]
+print(13 in a)
+print(7 not in a)
+
+
+# Identify operator
+
+a=3
+b=5
+print(a is b)
+print(a is not b)
+
+
