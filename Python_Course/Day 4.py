3addffe901a001ac11dbb3a95eea8ffe4404b62b
NADENDLA SAI TEJA <140083199+saiteja-4444@users.noreply.github.com>
2024-07-06 09:42:31 +0530

    Update Day 4.py

--- Python_Course/Day 4.py
+++ Python_Course/Day 4.py
@@ -1,1 +1,24 @@
+#                                                 PYTHON LISTS
+""" 
+LISTS: python list is used to store the various types of data 
+- python lists are mutable.We can modify the elements after it's created.
+- The in the list is sperated by (,)comma and enclosed with square brackets [].
+"""
 
+# list methods:
+- append()
+- copy()
+- clear()
+- count()
+- extend()
+- index()
+- insert()
+- pop()
+- remove()
+- reverse()
+- sort()
+
+
+# list comphression
+Syntax:
+print([Expression for item in iterable])
