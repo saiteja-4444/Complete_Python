711ac8a018c2759180a749963680ee8029b9c022
NADENDLA SAI TEJA <140083199+saiteja-4444@users.noreply.github.com>
2024-07-07 12:14:56 +0530

    Update Task 3.py

--- Daily_Tasks/Task 3.py
+++ Daily_Tasks/Task 3.py
@@ -17,3 +17,19 @@ while a<=n:
         b+=1
     print()
     a+=1
+
+
+# single table cretion using while loop
+
+a=int(input("enter your table:"))
+b=1
+while b<=10:
+    print(a,'*',b,'=',a*b)
+    b+=1
+
+
+# single tgable creatio using for loop
+n=int(input("Enter your table:"))
+for i in range(n,n+1):
+    for j in range(11):
+        print(i,'*',j,'=',i*j) 
