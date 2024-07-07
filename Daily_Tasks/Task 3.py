ba78ead069c498a8c7a3c0f85010c1121e428651
NADENDLA SAI TEJA <140083199+saiteja-4444@users.noreply.github.com>
2024-07-07 12:08:22 +0530

    Update Task 3.py

--- Daily_Tasks/Task 3.py
+++ Daily_Tasks/Task 3.py
@@ -1,2 +1,19 @@
-# write a program with for loop for table creation.
+# write a program for table creation using for loop.
 
+for i in range(1,11):
+    for j in range(11):
+        print(i,'*',j,'=',i*j)
+    print()
+
+
+# Write a program for table creation using while loop.
+
+ n=int(input("How many tables:"))
+a=1
+while a<=n:
+    b=1
+    while b<=10:
+        print(a,'*',b,'=',a*b)
+        b+=1
+    print()
+    a+=1
